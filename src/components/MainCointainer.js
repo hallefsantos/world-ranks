0c39f2666dd67a48f27b823b1b55cbe909389f75
hallefsantos <hallefsantos@gmail.com>
2021-03-22 21:44:25 -0300

    Fix Main Container padding

--- src/components/MainCointainer.js
+++ src/components/MainCointainer.js
@@ -2,7 +2,7 @@ const MainCointainer = ({ children }) => {
     return (
         <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
             <div 
-                className="pb-20 px-5 w-full max-w-5xl mx-auto antialiased"
+                className="px-5 w-full max-w-5xl mx-auto antialiased"
             >
                 {children}
             </div>
