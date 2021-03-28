59d473e04a63187838905290a41b7a266ea14064
hallefsantos <hallefsantos@gmail.com>
2021-03-28 11:21:56 -0300

    Add ProgressBar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/tailwind.css'
+import NextNprogress from 'nextjs-progressbar'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#34D399"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="3"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
